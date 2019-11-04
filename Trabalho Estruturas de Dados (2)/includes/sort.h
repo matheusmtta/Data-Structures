7df14e173210d9ea4243c38e189f681931510300
matheusmtta <48037841+matheusmtta@users.noreply.github.com>
2019-11-04 14:36:09 -0300

    Add files via upload

---
+++ Trabalho Estruturas de Dados (2)/includes/sort.h
@@ -0,0 +1,12 @@
+#ifndef SORT_
+#define SORT_
+
+#include "planet.h"
+#include <string>
+#include <iostream>
+
+void Merge(Planet *arr, int left, int middle, int right);
+void Sort_Merge(Planet *arr, int left, int right);
+//void Radix()
+
+#endif
